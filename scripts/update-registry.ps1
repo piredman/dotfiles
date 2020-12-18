e452fbaf35fc06c569a4fd0ad5872c1123b3c0a3
Paul Redman <paul.i.redman@gmail.com>
2020-12-18 09:55:20 -0700

    cleanup

--- scripts/update-registry.ps1
+++
@@ -1,6 +0,0 @@
-Set-ItemProperty -Path "HKCU:\Software\Policies\Google\Chrome\ExtensionInstallBlacklist" -Name "1" -Value "HIINFRASTRUCTURE"
-Set-ItemProperty -Path "HKCU:\Software\Policies\Google\Chrome" -Name "BrowserAddPersonEnabled" -Value 1 -Type DWord
-Set-ItemProperty -Path "HKCU:\Software\Policies\Google\Chrome" -Name "BrowserGuestModeEnabled" -Value 1 -Type DWord
-Set-ItemProperty -Path "HKCU:\Software\Policies\Google\Chrome" -Name "SyncDisabled" -Value 0 -Type DWord
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "DisallowShaking" -Value 1 -Type DWord
-Write-Host "Registry Updated."
