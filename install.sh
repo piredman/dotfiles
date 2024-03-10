c45befce54ede686013532764e4ffb2119ff61ac
Paul Redman <454963+piredman@users.noreply.github.com>
2024-03-10 13:11:20 -0600

    Remove skhd and yabai

--- install.sh
+++ install.sh
@@ -5,12 +5,6 @@ ln -s ~/code/personal/dotfiles/tmux/.tmux.conf ~/.tmux.conf
 # .config
 ln -s ~/code/personal/dotfiles/nvim ~/.config/nvim
 
-mkdir ~/.config/skhd
-ln -s ~/code/personal/dotfiles/skhd/skhdrc ~/.config/skhd/skhdrc
-
-mkdir ~/.config/yabai
-ln -s ~/code/personal/dotfiles/yabai/yabairc ~/.config/yabai/yabairc
-
 # vscode
 ln -s ~/code/personal/dotfiles/vscode/settings.json "/Users/paulredman/Library/Application Support/Code/User/settings.json"
 ln -s ~/code/personal/dotfiles/vscode/keybindings.json "/Users/paulredman/Library/Application Support/Code/User/keybindings.json"
