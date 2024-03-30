a21e1b81dfd804cc51aaa0acb86b79f6c440f6f8
Paul Redman <454963+piredman@users.noreply.github.com>
2024-03-30 12:43:08 -0600

    Add hyprland conf

--- install-linux.sh
+++ install-linux.sh
@@ -1,8 +1,10 @@
 # home
-ln -s ~/dotfiles/zsh/.zshrc ~/.zshrc
+ln -s ~/dotfiles/git/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/wezterm/.wezterm.lua ~/.wezterm.lua
+ln -s ~/dotfiles/zsh/.zshrc ~/.zshrc
 
 # .config
+ln -s ~/dotfiles/archlinux/hypr ~/.config/hypr
 ln -s ~/dotfiles/nvim ~/.config/nvim
 ln -s ~/dotfiles/starship/starship.toml ~/.config/starship.toml
