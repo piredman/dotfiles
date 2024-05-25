8fdc3cb98900ab2ba0fb8ea0862d7c1129cf2f0c
Paul Redman <454963+piredman@users.noreply.github.com>
2024-05-25 12:04:38 -0600

    linux: config updates

--- install-linux.sh
+++ install-linux.sh
@@ -3,9 +3,12 @@ ln -s ~/dotfiles/git/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/wezterm/.wezterm.lua ~/.wezterm.lua
 ln -s ~/dotfiles/zsh/.zshrc ~/.zshrc
+ln -s ~/dotfiles/archlinux/xorg/.xinitrc ~/.xinitrc
 
 # .config
 ln -s ~/dotfiles/archlinux/hypr ~/.config/hypr
+ln -s ~/dotfiles/archlinux/i3 ~/.config/i3
+ln -s ~/dotfiles/archlinux/waybar ~/.config/waybar
 ln -s ~/dotfiles/nvim ~/.config/nvim
 ln -s ~/dotfiles/starship/starship.toml ~/.config/starship.toml
 ln -s ~/dotfiles/wofi ~/.config/wofi
