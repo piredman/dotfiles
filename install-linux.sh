1ada6149bc840724d656fefaf139fe8dcd53ce3b
Paul Redman <454963+piredman@users.noreply.github.com>
2024-05-30 06:45:52 -0600

    linux: ricing i3, picom, polybar, rofi

--- install-linux.sh
+++ install-linux.sh
@@ -6,9 +6,16 @@ ln -s ~/dotfiles/zsh/.zshrc ~/.zshrc
 ln -s ~/dotfiles/archlinux/xorg/.xinitrc ~/.xinitrc
 
 # .config
-ln -s ~/dotfiles/archlinux/hypr ~/.config/hypr
-ln -s ~/dotfiles/archlinux/i3 ~/.config/i3
-ln -s ~/dotfiles/archlinux/waybar ~/.config/waybar
 ln -s ~/dotfiles/nvim ~/.config/nvim
 ln -s ~/dotfiles/starship/starship.toml ~/.config/starship.toml
-ln -s ~/dotfiles/wofi ~/.config/wofi
+
+# .config (archlinux/wayland/hyprland)
+ln -s ~/dotfiles/archlinux/hypr ~/.config/hypr
+ln -s ~/dotfiles/archlinux/waybar ~/.config/waybar
+ln -s ~/dotfiles/archlinux/wofi ~/.config/wofi
+
+# .config (archlinux/x11/i3)
+ln -s ~/dotfiles/archlinux/i3 ~/.config/i3
+ln -s ~/dotfiles/archlinux/picom ~/.config/picom
+ln -s ~/dotfiles/archlinux/polybar ~/.config/polybar
+ln -s ~/dotfiles/archlinux/rofi ~/.config/rofi
