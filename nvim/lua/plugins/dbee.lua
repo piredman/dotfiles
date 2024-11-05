82c23a5fcad3d8ed18b1dd70822db53605da978c
Paul Redman <454963+piredman@users.noreply.github.com>
2024-11-05 16:44:12 -0700

    tmux updates

---
+++ nvim/lua/plugins/dbee.lua
@@ -0,0 +1,19 @@
+--TODO: Try out dbee instead of dadbod
+return {
+  {
+    "kndndrj/nvim-dbee",
+    enabled = false,
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+    },
+    build = function()
+      -- Install tries to automatically detect the install method.
+      -- if it fails, try calling it with one of these parameters:
+      --    "curl", "wget", "bitsadmin", "go"
+      require("dbee").install()
+    end,
+    config = function()
+      require("dbee").setup(--[[optional config]])
+    end,
+  },
+}
