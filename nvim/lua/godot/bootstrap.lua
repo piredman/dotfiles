a061a45de119236082bd9a4a2e74f72eda00dead
Paul Redman <454963+piredman@users.noreply.github.com>
2025-04-13 10:04:24 -0600

    linux: config

--- nvim/lua/godot/bootstrap.lua
+++ nvim/lua/godot/bootstrap.lua
@@ -4,5 +4,7 @@ if vim.fn.filereadable(vim.fn.getcwd() .. '/project.godot') == 1 then
   if not vim.loop.fs_stat(pipepath) then
     vim.fn.serverstart(pipepath)
     vim.notify('Connected neovim as Godot external editor.', vim.log.levels.INFO)
+  else
+    vim.notify('Unable to connected neovim as Godot external editor.', vim.log.levels.WARN)
   end
 end
